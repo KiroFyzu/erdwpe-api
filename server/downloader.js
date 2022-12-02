f80d03f5e59fb9d46910c0ed03066878cc0b5335
erdwpe <90571955+erdwpe@users.noreply.github.com>
2022-12-02 23:26:13 +0700

    Update downloader.js

--- server/downloader.js
+++ server/downloader.js
@@ -6,7 +6,7 @@ const hx = require('hxz-api');
 const { mediafireDl, twitter, ytPlayMp3, ytPlayMp4 } = require('../scraper/index'); 
 const tiktok = require("@xct007/tiktok-scraper");
 let { igApi } = require("insta-fetcher");
-let ig = new igApi("ds_user_id=4427199241;sessionid=4427199241%3AmRNPXYuHLPr2W6%3A29%3AAYfOpU-V2oBteARO7uZlpQw7kZ_09Q2oOxV1x9FePA")
+let ig = new igApi("ds_user_id=4427199241;sessionid=4427199241%3AxRayEm8eb2eRdW%3A29%3AAYeS-MIHWYbOJqRfezI0GAh5vGIyO-_QrX0tbN4RPA")
 
 
 router.get("/playmp3", async(req, res, next) => {
